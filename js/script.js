0410eb2595a32f8dc33f794a183b25f1717bb3b7
Davide Preti <davide.preti86@gmail.com>
2020-09-08 12:18:53 +0200

    HTML + logica click + risposta automatica OK

---
+++ js/script.js
@@ -0,0 +1,78 @@
+/* global $ */
+$(function () {
+
+
+    $("#user-menu > a").click(function () {
+        var userDrop = $("#user-menu").children(".dropdown");
+        $(userDrop).toggleClass("hide");
+
+        $("#user-menu > a > i").toggleClass("menu-active");
+    });
+
+
+    // $("#chat-menu > i").click(function () {
+    //     var chatDrop = $("#chat-menu").children(".dropdown");
+    //     $(chatDrop).toggleClass("hide");
+
+    //     $("#chat-menu > i").toggleClass("menu-active");
+    // });
+
+
+    // --------------------- TEST 
+    $("#input-chat__input").keyup(function () {
+
+        if (event.which == 13) {
+            var inputValue = $("#input-chat__input").val();
+
+            if (inputValue != "") {
+
+                var element = $(".template > div.my-message").clone();
+                element.prepend(inputValue);
+                $(".active-chat").append(element);
+                $("#input-chat__input").val("");
+                $(".chat-info__member").html("Sta scrivendo...");
+
+            }
+
+            var rispostaAutomatica = setTimeout(function () {
+                var otherElement = $(".template > div.other-message").clone();
+                otherElement.prepend("Ciao, come stai?");
+                $(".active-chat").append(otherElement);
+
+                var date = new Date();
+                var hours = date.getHours();
+                var minute = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+
+                $(".chat-info__member").html(`Ultimo accesso alle ${hours}:${minute}`);
+
+            }, 2500);
+
+        }
+
+    });
+    // --------------------- /TEST
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
